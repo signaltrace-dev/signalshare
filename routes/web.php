adbf04d3fdbc8fd8860b732e936757dda0286734
Jeff Glenn <jeff@signaltrace.net>
2017-01-14 22:47:36 -0500

    Group routes and remove deprecated routes

--- routes/web.php
+++ routes/web.php
@@ -11,23 +11,19 @@
 |
 */
 
-
-
 Route::model('projects', 'Project');
 Route::model('tracks', 'Track');
-Route::model('files', 'AudioFile');
 
 Route::group(['middleware' => 'auth'], function(){
+	// Dashboards
 	Route::get('/', 'DashboardController@index');
 
-	Route::get('projects/{project}/tracks/create/{ajax?}', ['as' => 'create_track_ajax', 'uses' => 'TracksController@create']);
-	Route::post('files/add', ['as' => 'create_file', 'uses' => 'AudioFilesController@storeTemp']);
-	Route::post('files/delete', ['as' => 'delete_file', 'uses' => 'AudioFilesController@deleteTemp']);
-
+	// Projects
 	Route::resource('projects', 'ProjectController');
-	Route::resource('projects.tracks', 'TrackController');
-	Route::resource('files', 'AudioFilesController');
 
+	// Tracks
+	Route::resource('projects.tracks', 'TrackController');
+	Route::get('projects/{project}/tracks/create/{ajax?}', ['as' => 'create_track_ajax', 'uses' => 'TrackController@create']);
 	Route::get('tracks', ['as' => 'tracks.index', 'uses' => 'TrackController@index']);
 	Route::delete('tracks/{track}', ['as' => 'tracks.destroy', 'uses' => 'TrackController@destroy']);
 	Route::delete('projects/{project}/tracks/{track}', ['as' => 'projects.tracks.destroy', 'uses' => 'TrackController@removeFromProject']);
