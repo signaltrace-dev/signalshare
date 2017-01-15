c023cc5fb1b51c91bfc93ffcecb9ac2a39bc8d7b
Jeff Glenn <jeff@signaltrace.net>
2017-01-14 22:30:06 -0500

    Use slug as route key

--- app/Project.php
+++ app/Project.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
+    public function getRouteKeyName(){
+        return 'slug';
+    }
     protected $guarded = [];
     //
     public function tracks()
