7dc108a6f3478393293705e33506b24b8dd34890
Jeff Glenn <jeff@signaltrace.net>
2017-01-18 08:23:42 -0500

    Use addTrack function

--- resources/assets/js/tracks/uploader.js
+++ resources/assets/js/tracks/uploader.js
@@ -4,62 +4,28 @@ Dropzone.options.fileupload = {
         $('.dz-message').hide();
         formData.append("_token", $('[name=_token').val());
     },
-    queuecomplete: function(event){
-        $('.dz-complete').fadeOut(function(){
+    queuecomplete: function(event) {
+        $('.dz-complete').fadeOut(function() {
             $('.dz-message').fadeIn();
         });
     },
-    success: function(event, response){
-        if(response.status && response.status == 1){
+    success: function(event, response) {
+        if (response.status && response.status == 1) {
             var project_slug = response.project.slug;
             var track_slug = response.track.slug;
             var file_url = response.file.filename;
 
+            // Get track content and add to track list
             $.ajax({
                 url: '/projects/' + project_slug + '/tracks/' + track_slug,
-                type:'GET',
-                success: function(data){
-                    var fileItem = $('<li/>', {'class' : 'track-item added'});
-
-                    $(data).appendTo(fileItem);
-                    var src = '/files/' + file_url;
-                    $.ajax({
-                        url:src,
-                        type:'HEAD',
-                        success: function()
-                        {
-                            var trackElem = $(fileItem).find('.project-track').first()[0];
-
-                          var wavesurfer = Object.create(WaveSurfer);
-
-                          wavesurfer.init({
-                              container: trackElem,
-                              waveColor: '#A6C4FF',
-                              progressColor: '#89B0FF',
-                          });
-
-                          wavesurfer.on('play', function(){
-                          });
-
-                          wavesurfer.load(src);
-                          $(trackElem).addClass('loaded');
-                          $(trackElem).parent().find('.btn-play').first().removeClass('hidden');
-                        }
-                    });
-
-                    $(fileItem).prependTo('#track-list');
-                    setTimeout(function(){
-                        $(fileItem).addClass('processed');
-                    }, 500);
-
-                    $('.alert-warning').fadeOut();
-                    $('.controls').removeClass('hidden');
+                type: 'GET',
+                success: function(data) {
+                    var fileItem = $('<div/>').html(data).contents();
+                    var trackElem = fileItem.first('.track-item');
+                    $(trackElem).data('src', '/files/' + file_url);
+                    signalsharePlayer.addTrack(trackElem, true);
                 }
-              });
+            });
         }
     },
 };
-
-$(document).ready(function(){
-    $('#btn-submit').hide();
-});
