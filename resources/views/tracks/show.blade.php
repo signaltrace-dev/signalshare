ff7e8ceca5e88d8bc5a289c80276aeedd0faf0a8
Jeff Glenn <jeff@signaltrace.net>
2017-01-18 08:22:00 -0500

    Use columns for track display

--- resources/views/tracks/show.blade.php
+++ resources/views/tracks/show.blade.php
@@ -1,11 +1,16 @@
-<span class='controls-inline'>
-  <button class='btn btn-info btn-mute'>Mute <i class='fa fa-volume-off'></i></button>
-  <form action='{{ route("projects.tracks.destroy", [$project->slug, $track->slug] ) }}' method="POST">
-      {{ method_field('DELETE') }}
-      {{ csrf_field() }}
-      <button type="submit" class="btn btn-danger">Remove</button>
-  </form>
-</span>
-<a class='track-title'>{{ $track->name }}</a>
-
-<div class='project-track' data-src='/files/{{ !empty($track->file) ? $track->file->filename : '' }}'></div>
+<div class="track-item row" data-src='/files/{{ !empty($track->file) ? $track->file->filename : '' }}'>
+    <div class="col col-md-1">
+        <span class='controls-inline'>
+          <button class='btn btn-info btn-mute'>Mute <i class='fa fa-volume-off'></i></button>
+          <form action='{{ route("projects.tracks.destroy", [$project->slug, $track->slug] ) }}' method="POST">
+              {{ method_field('DELETE') }}
+              {{ csrf_field() }}
+              <button type="submit" class="btn btn-danger">Remove</button>
+          </form>
+        </span>
+    </div>
+    <div class="col-md-11">
+        <a class='track-title'>{{ $track->name }}</a>
+        <div class='project-track'></div>
+    </div>
+</div>
