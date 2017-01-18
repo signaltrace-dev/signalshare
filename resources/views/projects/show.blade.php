531f7ffb2fa2e775610d9868d27fafa422811009
Jeff Glenn <jeff@signaltrace.net>
2017-01-17 22:31:34 -0500

    Add compiled tracks.js

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -2,9 +2,7 @@
 
 @section('content')
 
-@include('partials/_modal', ['modal_title' => 'Add Track', 'modal_id' => 'modal-create-track'])
-
-<script type="text/javascript" src="{{ URL::asset('js/wavesurfer.min.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('js/tracks.js') }}"></script>
 <div id="pnl-project-tracks">
     <h2>{{ $project->name }}</h2>
 
@@ -22,7 +20,5 @@
     <div class="controls {{ count($project->tracks) == 0 ? 'hidden' : '' }}">
         <span class="btn btn-info btn-play-all"><span>Play All</span>&nbsp;<i class="fa fa-play"></i></span>
     </div>
-
-
 </div>
 @endsection
