8f4b42b0510c680318539f94ab00392e55be931a
Jeff Glenn <jeff@signaltrace.net>
2017-01-18 22:53:37 -0500

    Show # of tracks on project listing

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -10,10 +10,13 @@
       There aren't any projects...yet.
     </div>
   @else
-  <div class="list-group projects">
+  <div class="list-group project-list">
       @foreach( $projects as $project )
           <a class="list-group-item" href="{{ route('projects.show', $project->slug) }}">
               {{ $project->name }}
+              @if ($project->totalTracks() > 0)
+                  <span class="badge">{{ $project->totalTracks() }} {{ $project->totalTracks() > 1 ? 'tracks' : 'track' }} </span>
+              @endif
           </a>
       @endforeach
   </div>
