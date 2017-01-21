2562fb1bffde883d66655c26c57ea06a0f57aaba
Jeff Glenn <jeff@signaltrace.net>
2017-01-20 22:17:31 -0500

    Move Vue-enabled player to partial

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -6,20 +6,22 @@
 @endsection
 
 @section('content')
+    <script type="text/javascript" src="{{ URL::asset('js/tracks.js') }}"></script>
+    <script type="text/javascript">
+        signalsharePlayer.project = "{{ $project->slug }}";
+    </script>
 
-<script type="text/javascript" src="{{ URL::asset('js/tracks.js') }}"></script>
-<div id="pnl-project-tracks">
-    @if ( count($project->tracks) == 0 )
-        <div class='alert alert-warning'>Hey, it looks like this project doesn't have any tracks yet. Get cracking!</div>
-    @endif
-    @include('tracks/forms/add_tracks', ['submit_text' => 'Create Track', 'project' => $project, 'action' => route("projects.tracks.store", $project->slug)])
-    <div class='track-list' id="track-list">
-        @foreach( $project->tracks as $track)
-              @include('tracks/show', ['track' => $track, 'project' => $project])
-        @endforeach
+    <div id="pnl-project-tracks">
+        @if ( count($project->tracks) == 0 )
+            <div class='alert alert-warning'>Hey, it looks like this project doesn't have any tracks yet. Get cracking!</div>
+        @endif
+        @include('tracks/forms/add_tracks', ['submit_text' => 'Create Track', 'project' => $project, 'action' => route("projects.tracks.store", $project->slug)])
+        <div class='track-list' id="track-list">
+            @foreach( $project->tracks as $track)
+                  @include('tracks/show', ['track' => $track, 'project' => $project])
+            @endforeach
+        </div>
+
+        @include('projects.player')
     </div>
-    <div class="controls {{ count($project->tracks) == 0 ? 'hidden' : '' }}">
-        <span class="btn btn-success btn-play-all"><span>Play All</span>&nbsp;<i class="fa fa-play"></i></span>
-    </div>
-</div>
 @endsection
