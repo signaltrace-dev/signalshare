85da0209e36a18ce3da18c849b9d0720b7d0ae55
Jeff Glenn <jeff@signaltrace.net>
2017-01-20 22:35:46 -0500

    Use Aldrich for time display

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,12 @@
 <html>
     <head>
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-      <meta name="csrf-token" content="{{ csrf_token() }}">
-      <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-      <script type="text/javascript" src="{{ URL::asset('js/app.js') }}"></script>
-      <title>signal.share() @hasSection('title') - @yield('title')@endif</title>
+        <link href="https://fonts.googleapis.com/css?family=Aldrich" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script type="text/javascript" src="{{ URL::asset('js/app.js') }}"></script>
+        <title>signal.share() @hasSection('title') - @yield('title')@endif</title>
     </head>
     <body class="{{$classes}}" id="app">
         <div class="container-fluid">
