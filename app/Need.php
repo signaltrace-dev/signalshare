4dd1b0e66c0db3d81cb2d23c4938fe19527eaea2
Jeff Glenn <jeff@signaltrace.net>
2017-01-24 23:24:43 -0500

    Add Project relationships

--- app/Need.php
+++ app/Need.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Need extends Model
 {
-    //
+    public function needs(){
+        return $this->belongsToMany('App\Project')
+          ->withPivot('user_id')
+          ->withTimestamps();
+    }
 }
