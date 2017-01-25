dd8481109bbea1e9f0971c8a577d0a2cf8a97519
Jeff Glenn <jeff@signaltrace.net>
2017-01-24 23:42:17 -0500

    Add table name for needs

--- app/Need.php
+++ app/Need.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Need extends Model
 {
+    protected $table = 'project_needs';
     public function needs(){
         return $this->belongsToMany('App\Project')
           ->withPivot('user_id')
