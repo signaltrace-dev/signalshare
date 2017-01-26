17519b42761bf8e0d25221c35e417c3cea9f2215
Jeff Glenn <jeff@signaltrace.net>
2017-01-26 10:31:47 -0500

    Default to Tags taxonomy on Tag save, creating if it does not exist

--- app/Tag.php
+++ app/Tag.php
@@ -6,7 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    public function tags(){
+    public function save(array $options = [])
+    {
+       // before save code
+       if(empty($this->taxonomy_id)){
+           $default_taxonomy = Taxonomy::where('name', 'Tags')->first();
+           if(empty($default_taxonomy)){
+              $default_taxonomy = new Taxonomy();
+              $default_taxonomy->name = 'Tags';
+              $default_taxonomy->slug = 'tags';
+              $default_taxonomy->user_id = 1;
+              $default_taxonomy->save();
+           }
+           $this->taxonomy_id = $default_taxonomy->id;
+       }
+       parent::save();
+       // after save code
+    }
+
+    public function projects(){
         return $this->belongsToMany('App\Project')
           ->withPivot('user_id')
           ->withTimestamps();
