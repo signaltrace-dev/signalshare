616cd8534ccd3651ae980a980bb230a33adc0dd1
Jeff Glenn <jeff@signaltrace.net>
2017-01-26 10:27:07 -0500

    Remove custom table name

--- app/Need.php
+++ app/Need.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Need extends Model
 {
-    protected $table = 'project_needs';
     public function needs(){
         return $this->belongsToMany('App\Project')
           ->withPivot('user_id')
