96c506242d9143ebbe7f66c1b14d3bd8aa36be03
Jeff Glenn <jeff@signaltrace.net>
2017-01-26 10:33:09 -0500

    Use new Tagger

--- resources/views/taxonomies/tags/forms/tag_picker.blade.php
+++ resources/views/taxonomies/tags/forms/tag_picker.blade.php
@@ -1,4 +1,3 @@
-<script type="text/javascript" src="{{ URL::asset('js/tags.js') }}"></script>
-<div id="tagger">
-    <tagger targetid='{{ $project->id }}' targetname='{{ $project->slug }}' targettype="projects"></tagger>
+<div id="tag-tagger">
+    <tagger targetobj='{{ $project }}'></tagger>
 </div>
