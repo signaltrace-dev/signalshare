a791ad42c331536c721a259dc6f892a98eb6e6da
Jeff Glenn <jeff@signaltrace.net>
2017-01-25 20:17:54 -0500

    Remove default vals for need references

--- database/migrations/2017_01_25_041441_create_project_needs_tables.php
+++ database/migrations/2017_01_25_041441_create_project_needs_tables.php
@@ -16,7 +16,7 @@ class CreateProjectNeedsTables extends Migration
         Schema::create('project_needs', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name')->default('');
-            $table->integer('user_id')->unsigned()->default(0);
+            $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
@@ -24,11 +24,11 @@ class CreateProjectNeedsTables extends Migration
         Schema::create('project_project_needs', function(Blueprint $table){
             $table->increments('id');
             $table->timestamps();
-            $table->integer('project_id')->unsigned()->default(0);
+            $table->integer('project_id')->unsigned();
             $table->foreign('project_id')->references('id')->on('projects');
-            $table->integer('need_id')->unsigned()->default(0);
+            $table->integer('need_id')->unsigned();
             $table->foreign('need_id')->references('id')->on('project_needs');
-            $table->integer('user_id')->unsigned()->default(0);
+            $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
         });
     }
