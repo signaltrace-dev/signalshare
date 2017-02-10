c67177a55ad3631bcd5cfec01347fcbd57afec7b
Jeff Glenn <jeff@signaltrace.net>
2017-02-10 15:32:47 -0500

    Set error class on form-group

--- resources/views/projects/forms/create_edit.blade.php
+++ resources/views/projects/forms/create_edit.blade.php
@@ -1,9 +1,9 @@
 <form action='{{ $form_action }}' method="POST" class="form-inline">
     {{ method_field($form_method) }}
     {{ csrf_field() }}
-    <div class="form-group">
+    <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
         <label class="sr-only" for="name">Name:</label>
-        <input type="text" name="name"  placeholder="Yakety Sax Volume 2" value="{{ !empty($project) ? $project->name : '' }}" />
+        <input class="form-control" type="text" name="name"  placeholder="Yakety Sax Volume 2" value="{{ !empty($project) ? $project->name : '' }}" />
     </div>
     <div class="form-group">
         <button type="submit" class="btn btn-primary">{{ $submit_text }}</button>
