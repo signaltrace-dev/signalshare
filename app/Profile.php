cd5aa8cf640d75c22044ebec6e64d460f5a442e2
Jeff Glenn <jeff@signaltrace.net>
2017-02-10 14:19:40 -0500

    Get image paths from config

--- app/Profile.php
+++ app/Profile.php
@@ -10,4 +10,18 @@ class Profile extends Model
     {
         return $this->belongsTo('App\User');
     }
+
+    public function getImageUrlAttribute($value)
+    {
+        $base_path = config('settings.profile_image_base');
+        return $base_path . $value;
+    }
+
+    public function title(){
+        $this->load('user');
+
+        $full_name = !empty($this->first_name) ? $this->first_name : '';
+        $full_name = !empty($this->last_name) ? $full_name . ' ' . $this->last_name : $full_name;
+        return $full_name;
+    }
 }
