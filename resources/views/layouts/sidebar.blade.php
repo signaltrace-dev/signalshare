8a7ea61b33d77fdee1dcbbdc65d89824fa1ab6d6
Jeff Glenn <jeff@signaltrace.net>
2017-02-12 19:52:35 -0500

    Sidebar button styling

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -6,9 +6,9 @@
         @yield('sidebar', 'Sidebar')
     @else
         <div class="btn-group-vertical sidenav" role="group" aria-label="Main navigation">
-            <a href="{{ route('projects.my') }}" class="btn btn-default {{ strpos(Route::currentRouteName(), 'projects') === 0 ? 'active' : '' }}">Projects</a>
-            <a href="{{ route('tracks.index') }}" class="btn btn-default {{ strpos(Route::currentRouteName(), 'tracks') === 0 ? 'active' : '' }}">Tracks</a>
-            <a href="{{ route('taxonomies.index') }}" class="btn btn-default {{ strpos(Route::currentRouteName(), 'taxonomies') === 0 ? 'active' : '' }}">Taxonomies</a>
+            <a href="{{ route('projects.my') }}" class="btn btn-brand {{ strpos(Route::currentRouteName(), 'projects') === 0 ? 'active' : '' }}">Projects</a>
+            <a href="{{ route('tracks.index') }}" class="btn btn-brand {{ strpos(Route::currentRouteName(), 'tracks') === 0 ? 'active' : '' }}">Tracks</a>
+            <a href="{{ route('taxonomies.index') }}" class="btn btn-brand {{ strpos(Route::currentRouteName(), 'taxonomies') === 0 ? 'active' : '' }}">Taxonomies</a>
         </div>
     @endif
 </div>
