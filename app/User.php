1fe40543782492eb5f9097e06c516efeddc743bd
Jeff Glenn <jeff@signaltrace.net>
2017-02-13 11:29:31 -0500

    Change route key name

--- app/User.php
+++ app/User.php
@@ -9,6 +9,11 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    public function getRouteKeyName()
+    {
+        return 'name';
+    }
+
     /**
      * The attributes that are mass assignable.
      *
