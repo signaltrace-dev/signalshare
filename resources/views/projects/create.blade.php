c93487053dd0055926776418af49820a62f0630d
Jeff Glenn <jeff@signaltrace.net>
2017-02-15 21:19:06 -0500

    Remove old views

--- resources/views/projects/create.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts/app')
-
-@section('content')
-    <h2>Create Project</h2>
-    @include('projects/forms/create_edit', ['submit_text' => 'Create Project', 'form_action' => route("projects.store"), 'form_method' => 'POST'])
-@endsection
--- resources/views/projects/edit.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts/app')
-
-@section('content')
-    <h2>Edit Project</h2>
-    @include('projects/forms/create_edit', ['submit_text' => 'Save', 'form_action' => route("projects.update", $project->slug), 'form_method' => 'PATCH'])
-@endsection
