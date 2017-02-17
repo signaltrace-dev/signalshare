02f8f21547e15ad3c79a34ddebe08e8067d6e93f
Jeff Glenn <jeff@signaltrace.net>
2017-02-17 15:26:04 -0500

    Add help text for tagger

--- resources/views/needs/forms/need_tagger.blade.php
+++ resources/views/needs/forms/need_tagger.blade.php
@@ -1,3 +1,9 @@
 <div id="need-tagger">
     <tagger targetobj='{{ $project }}' tagtype="needs"></tagger>
+    <br>
+    <article class="message is-primary">
+        <div class="message-body">
+            Add Needs for things that your project needs or that you're looking for help with! ('Vocals', 'Bass', 'Didgeridoo', etc.)"
+        </div>
+    </article>
 </div>
--- resources/views/taxonomies/tags/forms/tag_picker.blade.php
+++ resources/views/taxonomies/tags/forms/tag_picker.blade.php
@@ -1,3 +1,9 @@
 <div id="tag-tagger">
     <tagger targetobj='{{ $project }}'></tagger>
+    <br>
+    <article class="message is-primary">
+        <div class="message-body">
+            Tag your project with anything that helps describe it to people who are looking for things they're interested in! ('Metal', 'Smooth Funk', 'Disco Normcore', etc.)
+        </div>
+    </article>
 </div>
